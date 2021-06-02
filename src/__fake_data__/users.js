3b5537a77c8a6fe951333fb25b45f30df64fa68b
Euclides Bernardo <euclidesquissembebernado@gmail.com>
2021-06-02 15:15:28 +0100

    :zap: Fake data users

---
+++ src/__fake_data__/users.js
@@ -0,0 +1,16 @@
+module.exports = {
+  users: [
+    {
+      id: 1,
+      name: "Euclides Bernardo",
+      email: "euclides.bernardo@nzooji.com",
+      age: 20,
+    },
+    {
+      id: 2,
+      name: "Raúl Inácio",
+      email: "raul.inacio@nzooji.com",
+      age: 21,
+    },
+  ],
+};
