1c13370cf8f0aab595138bbf7314f2bcced23960
Euclides Bernardo <euclidesquissembebernado@gmail.com>
2021-06-02 15:15:07 +0100

    :zap: Fake data contacts

---
+++ src/__fake_data__/contacts.js
@@ -0,0 +1,22 @@
+module.exports = {
+  contacts: [
+    {
+      id: 1,
+      user_id: 1,
+      code: "+244",
+      number: "926818886",
+    },
+    {
+      id: 2,
+      user_id: 1,
+      code: "+244",
+      number: "993597086",
+    },
+    {
+      id: 3,
+      user_id: 2,
+      code: "+244",
+      number: "926818886",
+    },
+  ],
+};
